20846906e11d2f10da6a506407d4c8a5a41656cf
JingyuYan <tunmxy@163.com>
2023-12-25 00:53:00 +0800

    urm sum

--- superposition_example.py
+++ superposition_example.py
@@ -1,10 +1,9 @@
-from urm_simulation import C, Z, J, S
-import urm_simulation as urm
+from urm_simulation import *
 
 
 def build_sum_triplet():
     # Original sum function: sum(x, y) = x + y; input_x -> r1, input_y -> r2
-    sum_instructions = urm.Instructions([
+    sum_instructions = Instructions([
         C(2, 0),
         Z(2),
         J(1, 2, 0),
@@ -15,7 +14,7 @@ def build_sum_triplet():
     # Copy the instructions to g1
     g1 = sum_instructions.copy()
     # Get highest from g1
-    highest_reg_sum = urm.haddr(g1)
+    highest_reg_sum = haddr(g1)
     L1 = 0
     n = 2  # two param
     # Used to calculate the relocation register address
@@ -27,12 +26,12 @@ def build_sum_triplet():
     print(alloc2)
 
     # The g2 instructions was obtained from location g1
-    g2 = urm.reloc(g1, alloc2)
+    g2 = reloc(g1, alloc2)
 
     # To keep the program nested-running, use a glue op to copy the results of g1 as input to g2
-    glue_op = urm.Instructions([C(L1, L2 + 1)])
+    glue_op = Instructions([C(L1, L2 + 1)])
     # To normalize the output, the output must start at index 0 or index 0
-    outputs_normal = urm.Instructions([C(L2, 0)])
+    outputs_normal = Instructions([C(L2, 0)])
     # Using the URM command connect function, assemble a sum(x,y,z)=x+y+z function
     sum_triplet = g1 + glue_op + g2 + outputs_normal
 
@@ -48,7 +47,7 @@ def build_sum_triplet():
 
 def build_sum_n(n: int = 3):
     # Basic function: add(x, y) = x + y
-    add_instructions = urm.Instructions([
+    add_instructions = Instructions([
         C(2, 0),
         Z(2),
         J(1, 2, 0),
@@ -67,7 +66,7 @@ def build_sum_n(n: int = 3):
         return add_instructions, inputs_index, outputs_index
 
     # The highest register index of Basic func add(x, y)
-    add_highest = urm.haddr(add_instructions)
+    add_highest = haddr(add_instructions)
     # Iter number
     numbers = tuple(range(3, n + 1))
 
@@ -82,19 +81,18 @@ def build_sum_n(n: int = 3):
     superposition = add_instructions.copy()
 
     for idx, nb in enumerate(numbers):
-        L_pre = L[idx+0]
-        L_cur = L[idx+1]
-        L_suc = L[idx+2]
+        L_pre = L[idx + 0]
+        L_cur = L[idx + 1]
+        L_suc = L[idx + 2]
         alloc = tuple(range(L_cur, L_suc))
 
-        g = urm.reloc(add_instructions, alloc)
-        glue_op = urm.Instructions([C(L_pre, L_cur + 1)])
-        outputs_normal = urm.Instructions([C(L_cur, 0)])
+        g = reloc(add_instructions, alloc)
+        glue_op = Instructions([C(L_pre, L_cur + 1)])
+        outputs_normal = Instructions([C(L_cur, 0)])
 
         superposition = superposition + glue_op + g + outputs_normal
 
         inputs_index.append(L_cur + 2)
-
 
     return superposition, inputs_index, outputs_index
 
@@ -104,14 +102,14 @@ def case_sum_triplet():
     print(sum_triplet)
     print(inputs_index, outputs_index)
     #
-    # registers = urm.allocate(urm.haddr(sum_triplet) + 1)
+    # registers = allocate(haddr(sum_triplet) + 1)
     # print(registers.summary())
     #
     # x = 5
     # y = 5
     # z = 12
     #
-    # result = urm.forward({inputs_index[0]: x, inputs_index[1]: y, inputs_index[2]: z}, registers, sum_triplet,
+    # result = forward({inputs_index[0]: x, inputs_index[1]: y, inputs_index[2]: z}, registers, sum_triplet,
     #                      safety_count=1000)
     # for idx, reg in enumerate(result.registers_of_steps):
     #     command = result.ops_of_steps[idx]
@@ -127,15 +125,15 @@ def case_sum_n():
     print(sum_instructions)
     print(f"inputs_index: {inputs_index}")
     print(f"outputs_index: {outputs_index}")
-    registers = urm.allocate(urm.haddr(sum_instructions) + 1)
+    registers = allocate(haddr(sum_instructions) + 1)
     print(registers.summary())
 
     inputs_data = [1, 2, 3, 4]
     assert len(inputs_data) == len(inputs_index)
     param = dict(zip(inputs_index, inputs_data))
     print(param)
-    result = urm.forward(param, registers, sum_instructions,
-                         safety_count=1000)
+    result = forward(param, registers, sum_instructions,
+                     safety_count=1000)
     for idx, reg in enumerate(result.registers_of_steps):
         command = result.ops_of_steps[idx]
         print(reg, command)
@@ -143,8 +141,9 @@ def case_sum_n():
     out = outputs_index[0]
     print(result.last_registers[out])
 
+
 def case_sub():
-    sum_instructions = urm.Instructions([
+    sum_instructions = Instructions([
         C(1, 4),
         J(3, 2, 14),
         J(5, 4, 10),
@@ -160,15 +159,45 @@ def case_sub():
         J(0, 0, 2),
         C(4, 0),
     ])
-    registers = urm.allocate(urm.haddr(sum_instructions) + 1)
+    registers = allocate(haddr(sum_instructions) + 1)
     x = 20
     y = 14
-    result = urm.forward({1: x, 2: y}, registers, sum_instructions,
-                         safety_count=1000)
+    result = forward({1: x, 2: y}, registers, sum_instructions,
+                     safety_count=1000)
     for idx, reg in enumerate(result.registers_of_steps):
         command = result.ops_of_steps[idx]
         print(reg, command)
 
+
+def build_gm(m: int):
+    sub_instructions = Instructions([
+        C(1, 4),
+        J(3, 2, 14),
+        J(5, 4, 10),
+        S(5),
+        J(5, 4, 9),
+        S(5),
+        S(0),
+        J(0, 0, 5),
+        Z(5),
+        S(3),
+        C(0, 4),
+        Z(0),
+        J(0, 0, 2),
+        C(4, 0),
+    ])
+    instructions_list = list()
+    param_num = 2
+    add_highest = haddr(sub_instructions)
+    L = [0]
+    for i in range(1, m):
+        L.append(L[-1] + max(param_num, add_highest) + 1)
+    print(L)
+    for idx in range(1, len(L)):
+        for pn in range(1, param_num+1):
+            instructions_list.append(C(pn, L[idx] + pn))
+
+    print(instructions_list)
 
 if __name__ == '__main__':
     # case_sub()
@@ -176,4 +205,5 @@ if __name__ == '__main__':
     #
     # build_sum_n(4)
 
-    case_sum_n()
+    # case_sum_n()
+    build_gm(m=5)
