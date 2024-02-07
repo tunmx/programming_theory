d2dc3bd3ce400a4e83e1dc5cd9d689e5da6aab79
JingyuYan <tunmxy@163.com>
2024-02-08 00:26:15 +0800

    Add some test

--- some_function/README.md
+++ some_function/README.md
@@ -8,7 +8,7 @@
 | 04   | pred(0) = 0 and pred(x + 1) = x                            | ✅ |             |
 | 05   | minus(x, y) = 0 whenever x < y and minus(x, y) = x - y     | ✅ |             |
 | 06   | sg(0) = 0 and sg(x) = 1 for other x                        | ✅ |             |
-| 07   | isg(0) = 1 and isg(x) = 0 for other x                      |  |             |
+| 07   | isg(0) = 1 and isg(x) = 0 for other x                      | ✅ |             |
 | 08   | lt(x, y) = 0 if x < y and lt(x, y) = 1 for other x and y   |  |             |
 | 09   | eq(x, y) = 0 if x = y and eq(x, y) = 1 for other x and y   |  |             |
 | 10   | gt(x, y) = 0 if x > y and gt(x, y) = 1 for other x and y   |  |             |
--- some_function/isg.py
+++ some_function/isg.py
@@ -8,19 +8,20 @@ sys.path.append(parent_dir)
 from urm_simulation import *
 
 
-sg_instruct = Instructions(
-    Z(2),
-    J(1, 2, 4),
+isg_instruct = Instructions(
     S(0),
+    Z(2),
+    J(1, 2, 5),
+    Z(0),
 )
 
-def sg(x, safety_count=10000):
-    num_of_registers = haddr(sg_instruct) + 1
+def isg(x, safety_count=10000):
+    num_of_registers = haddr(isg_instruct) + 1
     registers = allocate(num_of_registers)
     input_nodes = {1: x}
-    result = forward(input_nodes, registers, sg_instruct, safety_count=safety_count)
+    result = forward(input_nodes, registers, isg_instruct, safety_count=safety_count)
     return result.last_registers[0]
 
 if __name__ == '__main__':
-    print(f'sg(0)={sg(0)}')
-    print(f'sg(10086)={sg(10086)}')
+    print(f'isg(0)={isg(0)}')
+    print(f'isg(10086)={isg(10086)}')
